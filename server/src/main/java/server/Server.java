37011b001c94a4c3be856a4aab605cf31d2f336b
Angela Preece <angela.celeste.preece@gmail.com>
2025-10-13 15:07:44 -0600

    Updating server. Still passing Normal User Registration test.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,13 +1,17 @@
 package server;
 
 import com.google.gson.Gson;
+import datamodel.User;
 import io.javalin.*;
 import io.javalin.http.Context;
+import service.UserService;
+
 import java.util.Map;
 
 public class Server {
 
     private final Javalin server;
+    private UserService userService = new UserService();
 
     public Server() {
         server = Javalin.create(config -> config.staticFiles.add("web"));
@@ -15,16 +19,14 @@ public class Server {
         server.delete("db", ctx -> ctx.result("{}"));
         server.post("user", this::register);
 
-        // Register your endpoints and exception handlers here.
-
     }
 
     private void register(Context ctx) {
         var serializer = new Gson();
-        var req = serializer.fromJson(ctx.body(), Map.class);
-        req.put("authToken", "cow");
-        var res = serializer.toJson(req);
-        ctx.result(res);
+        var req = serializer.fromJson(ctx.body(), User.class);
+
+        var res = userService.register(req);
+        ctx.result(serializer.toJson(res));
     }
 
     public int run(int desiredPort) {
---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,11 @@
+package service;
+
+import datamodel.RegistrationResult;
+import datamodel.User;
+
+
+public class UserService {
+    public RegistrationResult register(User user) {
+        return new RegistrationResult(user.username(), "zzzy");
+    }
+}
