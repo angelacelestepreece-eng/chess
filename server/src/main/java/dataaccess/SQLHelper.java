b406222ee57f047a56b87a918e2ef5bc0239607b
Angela Preece <angela.celeste.preece@gmail.com>
2025-11-03 22:23:53 -0700

    Decomposing to help with code quality check.

--- server/src/main/java/dataaccess/SQLHelper.java
+++ server/src/main/java/dataaccess/SQLHelper.java
@@ -1,26 +1,28 @@
 package dataaccess;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 import static java.sql.Types.NULL;
 
 public class SQLHelper {
+
+    private static void params(PreparedStatement preparedStatement, Object... params) throws SQLException {
+        for (int i = 0; i < params.length; i++) {
+            Object param = params[i];
+            if (param instanceof String p) {
+                preparedStatement.setString(i + 1, p);
+            } else if (param instanceof Integer p) {
+                preparedStatement.setInt(i + 1, p);
+            } else if (param == null) {
+                preparedStatement.setNull(i + 1, NULL);
+            }
+        }
+    }
+
     public static int executeUpdate(String statement, Object... params) throws ResponseException {
         try (Connection conn = DatabaseManager.getConnection();
              var preparedStatement = conn.prepareStatement(statement)) {
-            for (int i = 0; i < params.length; i++) {
-                Object param = params[i];
-                if (param instanceof String p) {
-                    preparedStatement.setString(i + 1, p);
-                } else if (param instanceof Integer p) {
-                    preparedStatement.setInt(i + 1, p);
-                } else if (param == null) {
-                    preparedStatement.setNull(i + 1, NULL);
-                }
-            }
+            params(preparedStatement, params);
             return preparedStatement.executeUpdate();
         } catch (SQLException | DataAccessException e) {
             throw new ResponseException(ResponseException.Code.ServerError,
@@ -33,16 +35,7 @@ public class SQLHelper {
     public static int executeInsert(String statement, Object... params) throws ResponseException {
         try (Connection conn = DatabaseManager.getConnection();
              var preparedStatement = conn.prepareStatement(statement, Statement.RETURN_GENERATED_KEYS)) {
-            for (int i = 0; i < params.length; i++) {
-                Object param = params[i];
-                if (param instanceof String p) {
-                    preparedStatement.setString(i + 1, p);
-                } else if (param instanceof Integer p) {
-                    preparedStatement.setInt(i + 1, p);
-                } else if (param == null) {
-                    preparedStatement.setNull(i + 1, NULL);
-                }
-            }
+            params(preparedStatement, params);
             preparedStatement.executeUpdate();
             try (ResultSet rs = preparedStatement.getGeneratedKeys()) {
                 if (rs.next()) {
