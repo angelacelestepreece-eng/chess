b29a756e61642abc9f494e63e0bd46caf47dcbb2
Angela Preece <angela.celeste.preece@gmail.com>
2025-11-15 19:40:56 -0700

    Implemented logout and help for PostLoginClient, so now you can navigate within each loop.

--- client/src/main/java/ui/PostLoginClient.java
+++ client/src/main/java/ui/PostLoginClient.java
@@ -1,4 +1,78 @@
 package ui;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+import model.*;
+import exception.ResponseException;
+
+import static ui.EscapeSequences.*;
+
 public class PostLoginClient {
+    private final String username;
+    private State state = State.SIGNEDIN;
+
+    public PostLoginClient(String username) throws ResponseException {
+        this.username = username;
+    }
+
+    public void run() {
+        System.out.printf("Logged in as %s", username);
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = eval(line);
+                System.out.print(BLUE + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" + RESET + ">>> " + GREEN);
+    }
+
+
+    public String eval(String input) {
+        try {
+            String[] tokens = input.toLowerCase().split(" ");
+            String cmd = (tokens.length > 0) ? tokens[0] : "help";
+            String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "logout" -> logout();
+                case "quit" -> "quit";
+                default -> help();
+            };
+        } catch (ResponseException ex) {
+            return ex.getMessage();
+        }
+    }
+
+    public String logout() throws ResponseException {
+        state = State.SIGNEDOUT;
+        new PreLoginClient().run();
+        return "quit";
+    }
+
+
+    public String help() {
+        return """
+                create <NAME> - a game
+                list - games
+                join <ID> [WHITE|BLACK] - a game
+                observe <ID> - a game
+                logout - when you are done
+                quit - playing chess
+                help - with possible commands
+                """;
+    }
 }
+
